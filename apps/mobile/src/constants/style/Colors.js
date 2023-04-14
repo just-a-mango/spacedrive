6649a8104c2f7b34db4e5585767a9ca09c6f7874
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-04-14 10:36:33 +0300

    colors

---
+++ apps/mobile/src/constants/style/Colors.js
@@ -0,0 +1,67 @@
+module.exports = {
+	vanilla: {
+		primary: '#F5F5F5'
+	},
+	dark: {
+		// Brand blue
+		accent: {
+			DEFAULT: 'hsla(208, 100%, 47%, 1)',
+			faint: 'hsla(208, 100%, 64%, 1)',
+			deep: 'hsla(208, 100%, 47%, 1)'
+		},
+		ink: {
+			DEFAULT: 'hsla(230, 0%, 100%, 1)',
+			light: 'hsla(230, 0%, 82%, 1)',
+			dull: 'hsla(230, 10%, 70%, 1)',
+			faint: 'hsla(230, 10%, 55%, 1)'
+		},
+		app: {
+			DEFAULT: 'hsla(230, 15%, 13%, 1)',
+			// background (dark)
+			box: 'hsla(230, 15%, 17%, 1)',
+			darkBox: 'hsla(230, 15%, 7%, 1)',
+			// foreground (light)
+			overlay: 'hsla(230, 15%, 19%, 1)',
+			// border
+			line: 'hsla(230, 15%, 25%, 1)',
+			darkLine: 'hsla(230, 15%, 7%, 1)',
+			// 'selected' on desktop
+			highlight: 'hsla(230, 15%, 27%, 1)',
+			// shadow
+			shade: 'hsla(230, 15%, 0%, 1)',
+			// button
+			button: 'hsla(230, 15%, 23%, 1)',
+			// menu
+			menu: 'hsla(230, 25%, 5%, 1)',
+			// input
+			input: 'hsla(230, 15%, 20%, 1)'
+		},
+		sidebar: {
+			box: 'hsla(230, 15%, 16%, 1)',
+			line: 'hsla(230, 15%, 23%, 1)',
+			button: 'hsla(230, 15%, 18%, 1)'
+		},
+		gray: {
+			DEFAULT: '#505468',
+			50: '#F1F1F4',
+			100: '#E8E9ED',
+			150: '#E0E1E6',
+			200: '#D8DAE3',
+			250: '#D2D4DC',
+			300: '#C0C2CE',
+			350: '#A6AABF',
+			400: '#9196A8',
+			450: '#71758A',
+			500: '#303544',
+			550: '#20222d',
+			600: '#171720',
+			650: '#121219',
+			700: '#121317',
+			750: '#0D0E11',
+			800: '#0C0C0F',
+			850: '#08090D',
+			900: '#060609',
+			950: '#030303'
+		}
+	}
+};
--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,2 +1,1 @@
-// Note: Theme value is overridden by lib/tailwind.ts. It's there to make Eslint & VSCode happy.
-module.exports = require('./src/constants/style/tailwind')('dark');
+module.exports = require('./src/constants/style/tailwind.js')();
